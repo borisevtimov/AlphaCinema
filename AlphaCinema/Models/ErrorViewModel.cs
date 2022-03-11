b9b48e02055e4b9efc1ea1b563870932a10dae0f
borisevtimov <boriss.evtimov@gmail.com>
2022-03-11 15:43:46 +0200

    Add project files.

---
+++ AlphaCinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlphaCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
