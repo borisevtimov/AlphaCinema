4f0d9128f23dfe980ab9eae2bfcad10bad4f41ef
borisevtimov <boriss.evtimov@gmail.com>
2022-03-12 14:31:49 +0200

    Added constants for sections and views

---
+++ AlphaCinema/Constants/SectionConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace AlphaCinema.Constants
+{
+    public class SectionConstant
+    {
+        public const string Scripts = "Scripts";
+        public const string Headers = "Headers";
+    }
+}
---
+++ AlphaCinema/Constants/ViewConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace AlphaCinema.Constants
+{
+    public class ViewConstant
+    {
+        public const string Title = "Title";
+    }
+}
